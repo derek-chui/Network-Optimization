e7abb0e36e83e6c57e1e4dee9738cf4bdcb03eff
Derek Chui <151880171+derek-chui@users.noreply.github.com>
2025-05-13 01:15:46 -0700

    Update NOMA.m

--- NOMA.m
+++ NOMA.m
@@ -1,270 +1,319 @@
 %plan:
-%- make random points and weights
-%- sort these 12 points relative to origin (1 closest)
-%- make brute force triplet groupings O(n!!!) with points
-%- all grouping functions
-%- show all groupings on graph
+%- make random points
+%- sort these 10 points relative to origin (1 closest)
+%- cost matrix for hungarian
+%- make brute force pairing O(n!!) with points
+%- all pairing functions
+%- show all pairings on graph
 %- show results in command window
 %- HELPER FUNCTIONS
-%- all grouping functions
-%- calc score for grouping set
-%- generates all unique groupings (brute force) recursively
-%- plot groupings
-%- get weight for results
-%- print groupings
+%- all pairing functions
+%- calc tot dist for pairing set
+%- generates all unique pairings (brute force) recursively
+%- plot pairings
+%- print pairings
 %- compare results to brute force
 
-function NOMA3S()
-    numPoints = 12;
+
+function NOMA()
+    numPoints = 10;
     xRange = 10;
     yRange = 10;
     zRange = 10;
-
-    %make random points and weights
+    
+    %make random points
     x = rand(1, numPoints) * xRange;
     y = rand(1, numPoints) * yRange;
     z = rand(1, numPoints) * zRange;
-    weights = rand(1, numPoints) * 10;
     points = [x; y; z]';
 
-    %sort these 12 points relative to origin (1 closest)
+    %sort these 10 points relative to origin (1 closest)
     dist = sqrt(sum(points.^2, 2));
     [~, sortedIndices] = sort(dist);
     points = points(sortedIndices, :);
-    weights = weights(sortedIndices);
 
-    %make brute force triplet groupings O(n!!!) with points
-    pointIndices = 1:numPoints;
-    groupings = makeGroupings(pointIndices);
-    maxScore = -inf;
-    bestTriplets = [];
-    %get total scores
-    for i = 1:length(groupings)
-        groupingSet = groupings{i};
-        score = calcScore(groupingSet, points, weights);
-        if score > maxScore
-            maxScore = score;
-            bestTriplets = groupingSet;
+    %cost matrix for hungarian
+    %divide into close and far
+    close = 1:(numPoints/2);
+    far = (numPoints/2 + 1):numPoints;
+    %build cost matrix Cij = dist(i,j) or Ri + Rj (for now, use Euclidean dist)
+    costMatrix = zeros(length(close), length(far)); %#close x #far with 0
+    for i = 1:length(close) %iterate close and far users
+        for j = 1:length(far)
+            idxA = close(i); %get user indices
+            idxB = far(j);
+            %dist = cost of pairing two users
+            costMatrix(i, j) = norm(points(idxA,:) - points(idxB,:));
         end
     end
 
-    %all grouping functions
-    set1 = [1, 12, 2; 3, 11, 4; 5, 10, 6; 7, 9, 8];
-    set2 = [1, 2, 3; 4, 5, 6; 7, 8, 9; 10, 11, 12];
+    %make brute force pairing O(n!!) with points
+    pointIndices = 1:numPoints;
+    pairings = makePairings(pointIndices);
+    maxTotDist = -inf;
+    bestPairs = [];
+    %get total distances
+    for i = 1:length(pairings)
+        pairingSet = pairings{i};
+        %pairings has [Set 1], [Set 2], etc. which we got from makeParings
+        %pairing set is [Set i] = [1 3; 2 5; 4 6], for example
+        totDist = calcTotDist(pairingSet, points);
+        %update best pairing
+        if totDist > maxTotDist
+            maxTotDist = totDist;
+            bestPairs = pairingSet;
+        end
+    end
 
-    DNOMA = DNOMAGrouping(numPoints);
+    %all pairing functions
+    set1 = [1, 10; 2, 9; 3, 8; 4, 7; 5, 6];
+    totDist1 = calcTotDist(set1, points);
+    set2 = [1, 6; 2, 7; 3, 8; 4, 9; 5, 10];
+    totDist2 = calcTotDist(set2, points);
 
-    DNLUPA = DNLUPAGrouping(numPoints);
+    DNOMA = DNOMAPairing(numPoints);
+    totDistDNOMA = calcTotDist(DNOMA, points);
+    
+    DNLUPA = DNLUPAPairing(numPoints);
+    totDistDNLUPA = calcTotDist(DNLUPA, points);
+    
+    MUG = MUGPairing(numPoints);
+    totDistMUG = calcTotDist(MUG, points);
 
-    MUG = MUGGrouping(numPoints);
+    LCG = LCGPairing(points);
+    totDistLCG = calcTotDist(LCG, points);
+    DEC = DECPairing(points);
+    totDistDEC = calcTotDist(DEC, points);
 
-    LCG = LCGGrouping(points);
-    DEC = DECGrouping(points);
+    hungarianPairs = hungarianPairing(costMatrix, close, far);
+    totDistHungarian = calcTotDist(hungarianPairs, points);
+    jvPairs = JVPairing(costMatrix, close, far);
+    totDistJV = calcTotDist(jvPairs, points);
 
-    greedyTriplets = greedyGrouping(points, weights);
-
-    %show all groupings on graph
-    plotGroupings(points, bestTriplets, 'Brute Force');
-    plotGroupings(points, set1, 'Set 1');
-    plotGroupings(points, set2, 'Set 2');
-    plotGroupings(points, DNOMA, 'DNOMA');
-    plotGroupings(points, DNLUPA, 'DNLUPA');
-    plotGroupings(points, MUG, 'MUG');
-    plotGroupings(points, LCG, 'LCG');
-    plotGroupings(points, DEC, 'DEC');
-    plotGroupings(points, greedyTriplets, 'Greedy');
-
+    greedyPairs = greedyPairing(points);
+    totDistGreedy = calcTotDist(greedyPairs, points);
+    
+    %show all pairings on graph
+    plotPairings(points, bestPairs, 'Brute Force');
+    plotPairings(points, set1, 'Set 1');
+    plotPairings(points, set2, 'Set 2');
+    plotPairings(points, DNOMA, 'DNOMA');
+    plotPairings(points, DNLUPA, 'DNLUPA');
+    plotPairings(points, MUG, 'MUG');
+    plotPairings(points, LCG, 'LCG');
+    plotPairings(points, DEC, 'DEC');
+    plotPairings(points, hungarianPairs, 'Hungarian');
+    plotPairings(points, jvPairs, 'JV');
+    plotPairings(points, greedyPairs, 'Greedy');
+    
     %show results in command window
-    results.BruteForce = struct('triplets', bestTriplets, ...
-        'weights', getWeight(bestTriplets, weights), ...
-        'totalScore', maxScore, 'complexity', 'O(n!!!)');
-
-    results.set1 = struct('triplets', set1, ...
-        'weights', getWeight(set1, weights), ...
-        'totalScore', calcScore(set1, points, weights), 'complexity', 'O(1)');
-
-    results.set2 = struct('triplets', set2, ...
-        'weights', getWeight(set2, weights), ...
-        'totalScore', calcScore(set2, points, weights), 'complexity', 'O(1)');
-
-    results.DNOMA = struct('triplets', DNOMA, ...
-        'weights', getWeight(DNOMA, weights), ...
-        'totalScore', calcScore(DNOMA, points, weights), 'complexity', 'O(nlogn)');
-
-    results.DNLUPA = struct('triplets', DNLUPA, ...
-        'weights', getWeight(DNLUPA, weights), ...
-        'totalScore', calcScore(DNLUPA, points, weights), 'complexity', 'O(nlogn)');
-
-    results.MUG = struct('triplets', MUG, ...
-        'weights', getWeight(MUG, weights), ...
-        'totalScore', calcScore(MUG, points, weights), 'complexity', 'O(n)');
-
-    results.LCG = struct('triplets', LCG, ...
-        'weights', getWeight(LCG, weights), ...
-        'totalScore', calcScore(LCG, points, weights), 'complexity', 'O(nlogn)');
-
-    results.DEC = struct('triplets', DEC, ...
-        'weights', getWeight(DEC, weights), ...
-        'totalScore', calcScore(DEC, points, weights), 'complexity', 'O(nlogn)');
-
-    results.Greedy = struct('triplets', greedyTriplets, ...
-        'weights', getWeight(greedyTriplets, weights), ...
-        'totalScore', calcScore(greedyTriplets, points, weights), 'complexity', 'O(n^3)');
-
-    printTripletResults(results);
+    results.BruteForce     = struct('pairs', bestPairs,        'totalDist', maxTotDist,       'complexity', 'O(n!!)');
+    results.set1           = struct('pairs', set1,             'totalDist', totDist1,         'complexity', 'O(1)');
+    results.set2           = struct('pairs', set2,             'totalDist', totDist2,         'complexity', 'O(1)');
+    results.DNOMA          = struct('pairs', DNOMA,            'totalDist', totDistDNOMA,     'complexity', 'O(nlogn)');
+    results.DNLUPA         = struct('pairs', DNLUPA,           'totalDist', totDistDNLUPA,    'complexity', 'O(nlogn)');
+    results.MUG            = struct('pairs', MUG,              'totalDist', totDistMUG,       'complexity', 'O(n)');
+    results.LCG            = struct('pairs', LCG,              'totalDist', totDistLCG,       'complexity', 'O(nlogn)');
+    results.DEC            = struct('pairs', DEC,              'totalDist', totDistDEC,       'complexity', 'O(nlogn)');
+    results.Hungarian      = struct('pairs', hungarianPairs,   'totalDist', totDistHungarian, 'complexity', 'O(n^3)');
+    results.JV             = struct('pairs', jvPairs,          'totalDist', totDistJV,        'complexity', 'O(n^3)');
+    results.Greedy         = struct('pairs', greedyPairs,      'totalDist', totDistGreedy,    'complexity', 'O(n^3)');
+    
+    printPairingResults(results);
     compareBruteForce(results);
 end
 
 %HELPER FUNCTIONS
 
 %all pairing functions
-function triplets = DNOMAGrouping(N)
-    group1 = 1:4;
-    group2 = 5:8;
-    group3 = 9:12;
-    triplets = [group1(1), group2(1), group3(1);
-                group1(2), group2(2), group3(2);
-                group1(3), group2(3), group3(3);
-                group1(4), group2(4), group3(4)];
+function pairs = DNOMAPairing(N)
+    %divide into 4 groups
+    %FNN (First Nearest Near)
+    g1 = 1:floor(N/4);
+    %SNN (Second Nearest Near)
+    g2 = (floor(N/4)+1):floor(N/2);
+    %FFF (First Farthest Far)
+    g3 = (floor(N/2)+1):floor(3*N/4);
+    %SFF (Second Farthest Far)
+    g4 = (floor(3*N/4)+1:N);
+    %pair g1 with g3 (odd)
+    oddPairs = [];
+    len = min(length(g1), length(g3));
+    for i = 1:len
+        oddPairs = [oddPairs; g1(i), g3(i)];
+    end
+    %pair g2 with g4 (even)
+    evenPairs = [];
+    len = min(length(g2), length(g4));
+    for i = 1:len
+        evenPairs = [evenPairs; g2(i), g4(i)];
+    end
+    pairs = [oddPairs; evenPairs]; %combine
 end
 
-function triplets = DNLUPAGrouping(N)
-    % indices = 1:N;
-    % triplets = reshape(indices, 3, [])';
-    triplets = [1,6,12; 2,5,11; 3,8,10; 4,7,9];
+function pairs = DNLUPAPairing(N)
+    %divide into 2 groups
+    group1 = 1:floor(N/2);
+    group2 = (floor(N/2)+1):N;
+    
+    %pair first in g1 with last in g2 etc
+    pairs = [];
+    for i = 1:length(group1)
+        pairs = [pairs; group1(i), group2(end-i+1)];
+    end
 end
 
-function triplets = MUGGrouping(N)
-    triplets = reshape(1:N, 3, [])';
+function pairs = MUGPairing(N)
+    %pairing neighbours
+    pairs = [];
+    for i = 1:2:N-1 %1, 3, 5..
+        pairs = [pairs; i, i+1]; %pair 1 with 2, 3 with 4..
+    end
+    %if odd, last point pairs with nearest available earlier point
+    if mod(N,2) ~= 0
+        pairs(end,2) = N;
+    end
 end
 
-function triplets = LCGGrouping(points)
-    [~, order] = sort(sqrt(sum(points.^2, 2)));
-    triplets = reshape(order, 3, [])';
+function pairs = LCGPairing(points)
+    N = size(points, 1);
+    [~, order] = sort(sqrt(sum(points.^2, 2))); %sort by dist to origin
+    pairs = [];
+    for i = 1:2:N-1
+        pairs = [pairs; order(i), order(i+1)];
+    end
 end
-function triplets = DECGrouping(points)
-    strengths = sum(points, 2);
-    [~, sorted] = sort(strengths, 'descend');
-    triplets = reshape(sorted, 3, [])';
+function pairs = DECPairing(points)
+    N = size(points, 1);
+    strengths = sum(points, 2); %tot strength= x+y+z
+    [~, strong] = sort(strengths, 'descend');
+    [~, weak] = sort(strengths, 'ascend');
+    pairs = [];
+    for i = 1:N/2
+        pairs = [pairs; strong(i), weak(i)];
+    end
 end
 
-function triplets = greedyGrouping(points, weights)
-    indices = 1:size(points,1);
-    triplets = [];
-    while length(indices) >= 3
-        maxScore = -inf;
-        best = [];
+function pairs = hungarianPairing(costMatrix, close, far)
+    %invert and minimize tot cost, far = small cost, close = large cost
+    maxVal = max(costMatrix(:));
+    costMatrix = maxVal - costMatrix;
+    [match, ~] = matchpairs(costMatrix, 1e6); %make unmatch pairings expensive, so all should be matched
+    pairs = [close(match(:,1))', far(match(:,2))']; %close + far arrays
+end
+function pairs = JVPairing(costMatrix, close, far)
+    %matchpairs from optimization toolbox / custom LAP
+    %max score to min negative score
+    costMatrix = -costMatrix;
+    [match, ~] = matchpairs(costMatrix, -1); %-1 for max
+    pairs = [];
+    for i = 1:size(match,1) %pair clusters, match close row + far column
+        pairs = [pairs; close(match(i,1)), far(match(i,2))];
+    end
+end
+
+function pairs = greedyPairing(points)
+    N = size(points, 1);
+    indices = 1:N;
+    pairs = [];
+    while length(indices) > 1
+        maxDist = -inf;
+        bestPair = [];
+        %brute force check all remaining possible point pairs
         for i = 1:length(indices)
             for j = i+1:length(indices)
-                for k = j+1:length(indices)
-                    idx = [indices(i), indices(j), indices(k)];
-                    p = points(idx,:);
-                    w = weights(idx);
-                    distSum = norm(p(1,:) - p(2,:)) + norm(p(2,:) - p(3,:)) + norm(p(1,:) - p(3,:));
-                    weightDiff = max(w) - min(w);
-                    score = distSum + weightDiff;
-                    if score > maxScore
-                        maxScore = score;
-                        best = idx;
-                    end
+                idx1 = indices(i);
+                idx2 = indices(j);
+                dist = norm(points(idx1,:) - points(idx2,:));
+                if dist > maxDist %update if better
+                    maxDist = dist;
+                    bestPair = [idx1, idx2];
                 end
             end
         end
-        triplets = [triplets; best];
-        indices = setdiff(indices, best);
+        %save best pair, remove from list
+        pairs = [pairs; bestPair];
+        indices(indices == bestPair(1)) = [];
+        indices(indices == bestPair(2)) = [];
     end
 end
 
-%calc score for grouping set
-function score = calcScore(groupingSet, points, weights)
-    score = 0;
-    for i = 1:size(groupingSet,1)
-        idx = groupingSet(i,:);
-        p = points(idx,:);
-        w = weights(idx);
-
-        distSum = norm(p(1,:) - p(2,:)) + norm(p(2,:) - p(3,:)) + norm(p(1,:) - p(3,:));
-        weightDiff = max(w) - min(w);
-        score = score + distSum + weightDiff;
+%calc tot dist for pairing set
+function totDist = calcTotDist(pairingSet, points)
+    totDist = 0;
+    for j = 1:size(pairingSet, 1) %[1 3; 2 5; 4 6] = 3 loops
+        idx1 = pairingSet(j, 1); %[1 3] => 1
+        idx2 = pairingSet(j, 2); %[1 3] => 3
+        p1 = points(idx1, :); %1 => p1 = [2.5, 4.0]
+        p2 = points(idx2, :); %3 => p2 = [5.7, 7.8]
+        dist = norm(p1 - p2); %sqrt((x1-x2)^2+(y1-y^2))
+        totDist = totDist + dist;
     end
 end
 
-%generates all unique groupings (brute force) recursively
-function groupings = makeGroupings(indices)
-    if isempty(indices)
-        groupings = {[]};
+%generates all unique pairings (brute force) recursively
+function pairings = makePairings(indices) %(n-1)!! ways to form pairs = O(n!!) complexity
+    if isempty(indices) %base case
+        pairings = {[]};
         return;
     end
-    groupings = {};
+
+    pairings = {};
     first = indices(1);
-    for i = 2:length(indices)
-        for j = i+1:length(indices)
-            rest = indices([2:i-1, i+1:j-1, j+1:end]);
-            sub = makeGroupings(rest);
-            for k = 1:length(sub)
-                groupings{end+1} = [first, indices(i), indices(j); sub{k}];
-            end
+    for i = 2:length(indices) %loop over all possible choices for pairing with first
+        second = indices(i);
+        remaining = indices([2:i-1, i+1:end]); %exclude 1 2, get remaining
+        subPairings = makePairings(remaining); %all possible pairings with remaining (recursive)
+        for j = 1:length(subPairings) %each pair from recursion
+            pairing = [[first, second]; subPairings{j}]; %get [1 3; 2 5; 4 6] like example above
+            pairings{end+1} = pairing; %save this set to list of all possible pairings [Set 1], [Set 2], etc.
         end
     end
 end
 
-%plot groupings
-function plotGroupings(points, triplets, name)
+%plot pairings
+function plotPairings(points, pairs, name)
     figure;
     scatter3(points(:,1), points(:,2), points(:,3), 100, 'filled');
     hold on;
-    for i = 1:size(triplets,1)
-        idx = triplets(i,:);
-        plot3(points(idx,1), points(idx,2), points(idx,3), 'r-', 'LineWidth', 2);
-        plot3([points(idx(1),1), points(idx(3),1)], ...
-              [points(idx(1),2), points(idx(3),2)], ...
-              [points(idx(1),3), points(idx(3),3)], 'r-', 'LineWidth', 2);
+    for j = 1:size(pairs, 1)
+        idx1 = pairs(j, 1);
+        idx2 = pairs(j, 2);
+        plot3([points(idx1,1), points(idx2,1)], ...
+              [points(idx1,2), points(idx2,2)], ...
+              [points(idx1,3), points(idx2,3)], ...
+              'r-', 'LineWidth', 2);
     end
     xlabel('X'); ylabel('Y'); zlabel('Z');
-    title([name, ' Triplets']);
+    title([name, ' Pairing']);
     grid on;
 end
 
-%get weight for results
-function weightsMatrix = getWeight(triplets, weights)
-    weightsMatrix = zeros(size(triplets));
-    for i = 1:size(triplets, 1)
-        weightsMatrix(i, :) = weights(triplets(i, :));
-    end
-end
-
-%print groupings
-function printTripletResults(results)
+%print pairings
+function printPairingResults(results)
     fields = fieldnames(results);
     for i = 1:length(fields)
         key = fields{i};
-        tripletStruct = results.(key);
-        triplets = tripletStruct.triplets;
-        fprintf('\n%s Grouping (%s):\n', key, tripletStruct.complexity);
-        disp(triplets);
-        
-        fprintf('Weights for each triplet:\n');
-        for j = 1:size(triplets,1)
-            w = tripletStruct.weights(j,:);
-            fprintf('  Triplet %d: [%.2f, %.2f, %.2f]\n', j, w(1), w(2), w(3));
-        end
-        
-        fprintf('Total Score (Distance + Weight Difference): %.4f\n\n', tripletStruct.totalScore);
+        pairStruct = results.(key);
+        disp([key, ' Set ', pairStruct.complexity, ':']);
+        disp(pairStruct.pairs);
+        disp(['Total Distance: ', num2str(pairStruct.totalDist)]);
     end
 end
 
 %compare results to brute force
 function compareBruteForce(results)
-    bruteForceScore = results.BruteForce.totalScore;
+    bruteForceDist = results.BruteForce.totalDist;
     methods = fieldnames(results);
+
     disp(' ');
     for i = 1:length(methods)
         method = methods{i};
         if strcmp(method, 'BruteForce')
             continue;
         end
-        delta = bruteForceScore - results.(method).totalScore;
+        delta = bruteForceDist - results.(method).totalDist;
         disp(['Brute Force v ', method, '? ', num2str(delta)]);
     end
-    disp('The higher the difference, the more optimal brute force is for (distance + weight difference).');
+    disp('The higher the numbers, the more effective the brute force algorithm is compared to other algorithms.');
 end
